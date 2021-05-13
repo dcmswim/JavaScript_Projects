04bcdb6efed6f9748f9b3c317e0c8db10b8a360d
David <dcmswim@gmail.com>
2021-05-12 20:50:37 -0700

    add files
    push to GitHub to submit assignment to instructor

---
+++ Basic_JavaScript_Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,25 @@
+window.alert("Welcome!");
+var Family = "The Meyers", Dad = "Ryan", Mom = "Deanna", Son = "David", Daughter = "Ashley"; //defines variables
+var Family = Family.fontcolor("blue"); //sets font color for variable
+var Dad = Dad.fontcolor("blue");
+var Mom = Mom.fontcolor("blue");
+var Son = Son.fontcolor("blue");
+var Daughter = Daughter.fontcolor("blue");
+
+function My_First_Function () { //defining and naming function
+    var str = "This text is green!"; //defining a variable and giving it a string value
+    var result = str.fontcolor("green"); //changing font color of string result
+    document.getElementById("Green_Text").innerHTML = result; //putting the value of result into HTML element with id "Green_Text"
+}
+
+document.write(Dad); //displays output of a variable
+
+
+var A = 15 //assigns variable a value
+var B = 20 //assigns variable a value
+document.write(A+B); //displays output of the addition of 2 variables
+
+
+
+
+
